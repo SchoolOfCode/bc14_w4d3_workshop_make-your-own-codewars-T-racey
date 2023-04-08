34d0b05b03f2eae0b2af3f70b4791db8b954f602
Tracey Williams <traceywilliams@msn.com>
2023-04-08 12:53:18 +0100

    TRacey's first co

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 /* 
-👉 Write your kata here!
-*/
+You are a coach for a professional bicycle racing team, 
+and you need to analyse the results of the team's recent time trial. 
+The time trial consisted of a single stage, and for each rider on the team, 
+you have recorded the time that they took to complete the stage.
+Write a function calculateAverageTime(results) that takes an array results as input, 
+where results[i] represents the time (in minutes) that rider i took to complete the 
+stage. Your function should calculate the average time taken by all riders and return 
+it as a string with two decimal places.
 
-//👉 Write the function your CodeWarriors will start with below here:
+For example, if the input results array is:
+
+[20, 22, 24, 26, 28]
+
+This represents a time trial with 5 riders on the team. Rider 1 
+completed the stage in 20 minutes, rider 2 in 22 minutes, and so on. 
+
+Assume that the number of riders on the team is between 1 and 10, 
+and that the times are all positive integers.
+
+👉//write your code here.
+  function calculateAverageTime(arr) {
+{
+
+//Your function should calculate the average time taken by all riders and be returned as a string with two decimal places:
+"24.00"
+
--- main.test.js
+++ main.test.js
@@ -1,1 +1,43 @@
 //👉 Write your tests below here:
+import { test, expect } from "@jest/globals";
+import { calculateAverageTime } from './main.js';
+describe('calculateAverageTime', () => {
+    // Test to verify if the function correctly calculates the average time for a given input array
+    test('calculates the average time correctly', () => {
+      expect(calculateAverageTime([20, 22, 24, 26, 28])).toBe('24.00');
+      expect(calculateAverageTime([10, 20, 30, 40])).toBe('25.00');
+      expect(calculateAverageTime([30, 50, 70])).toBe('50.00');
+    });
+  
+    // Test to verify if the function returns 0 when the input array is empty
+    test('returns 0 when the input array is empty', () => {
+      expect(calculateAverageTime([])).toBe('0.00');
+    });
+  
+    // Test to verify if the function returns the only element of the input array when it contains only one element
+    test('returns the only element of the input array when it contains only one element', () => {
+      expect(calculateAverageTime([10])).toBe('10.00');
+    });
+  
+    // Test to verify if the function throws an error when the input is not an array
+    test('throws an error when the input is not an array', () => {
+      expect(() => calculateAverageTime('invalid input')).toThrowError('Input must be an array');
+      expect(() => calculateAverageTime(123)).toThrowError('Input must be an array');
+      expect(() => calculateAverageTime({})).toThrowError('Input must be an array');
+    });
+  
+    // Test to verify if the function throws an error when the input array contains non-numeric values
+    test('throws an error when the input array contains non-numeric values', () => {
+      expect(() => calculateAverageTime([10, 20, '30', 40])).toThrowError('Array must contain only numeric values');
+      expect(() => calculateAverageTime([10, 20, NaN, 40])).toThrowError('Array must contain only numeric values');
+      expect(() => calculateAverageTime([10, 20, null, 40])).toThrowError('Array must contain only numeric values');
+    });
+
+    // Test to verify if the function throws an error when the input array contains more than 10 elements
+    test('throws an error when input array contains more than 10 elements', () => {
+        const input = [20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40];
+        expect(() => calculateAverageTime(input)).toThrow('The number of riders on the team must be between 1 and 10');
+      });
+      
+  });
+  
